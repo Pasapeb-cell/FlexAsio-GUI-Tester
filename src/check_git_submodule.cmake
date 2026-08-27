3f6791a65cec1805cd0829531df7d40fb10a8fef
Pasapeb-Cell <pasapeb1@gmail.com>
2026-08-27 12:27:42 -0600

    Fix installer staging and release packaging

--- src/check_git_submodule.cmake
+++ src/check_git_submodule.cmake
@@ -1,5 +1,8 @@
 function(check_git_submodule DIR)
-    file(GLOB DIR_FILES "${DIR}/*")
+	# Resolve relative to the caller's source list instead of the process working
+	# directory. Script-mode invocations (the installer) may run from the repo
+	# root, where a same-named submodule directory does not exist.
+    file(GLOB DIR_FILES "${CMAKE_CURRENT_LIST_DIR}/${DIR}/*")
 	if (DIR_FILES STREQUAL "")
 		message(WARNING "It looks like the '${DIR}' directory is empty. Did you forget to update git submodules?")
 		find_package(Git MODULE)
