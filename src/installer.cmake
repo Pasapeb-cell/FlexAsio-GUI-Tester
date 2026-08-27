1b83bee45a07bc845daa9f547ded795f8adc3df6
Pasapeb-Cell <pasapeb1@gmail.com>
2026-08-27 12:16:38 -0600

    Make installer script work from local checkout

--- src/installer.cmake
+++ src/installer.cmake
@@ -4,6 +4,12 @@ include("${CMAKE_CURRENT_LIST_DIR}/check_git_submodule.cmake")
 check_git_submodule(dechamps_CMakeUtils)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/dechamps_CMakeUtils")
+# Winget installs Inno Setup per-user by default.  Prefer that explicit path
+# when present; the bundled finder continues to cover machine-wide installs.
+if(NOT DEFINED InnoSetup_iscc_EXECUTABLE
+   AND EXISTS "$ENV{LOCALAPPDATA}/Programs/Inno Setup 6/ISCC.exe")
+    set(InnoSetup_iscc_EXECUTABLE "$ENV{LOCALAPPDATA}/Programs/Inno Setup 6/ISCC.exe")
+endif()
 find_package(InnoSetup MODULE REQUIRED)
 
 find_package(Git MODULE REQUIRED)
@@ -17,5 +23,8 @@ set(FLEXASIO_VERSION "${DISTRIBUTION_VERSION}")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/installer.in.iss" "${CMAKE_CURRENT_LIST_DIR}/out/installer.iss" @ONLY)
 include(execute_process_or_die)
 execute_process_or_die(
-    COMMAND "${InnoSetup_iscc_EXECUTABLE}" out/installer.iss /Oout/installer /FFlexASIO-GUI-Tester-${FLEXASIO_VERSION}-Setup
+    COMMAND "${InnoSetup_iscc_EXECUTABLE}"
+        "${CMAKE_CURRENT_LIST_DIR}/out/installer.iss"
+        "/O${CMAKE_CURRENT_LIST_DIR}/out/installer"
+        "/FFlexASIO-GUI-Tester-${FLEXASIO_VERSION}-Setup"
 )
